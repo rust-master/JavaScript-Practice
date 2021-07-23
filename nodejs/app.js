f60fbac61793f4f26f7bd712bd049d7b6a2a463c
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-07-23 18:14:01 +0500

    modules in JS

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,3 +3,9 @@
 // file system
 // events
 // http
+
+// Modules
+// define module to constant to avoid exception
+const log = require('./logger');
+
+log('message');
---
+++ nodejs/logger.js
@@ -0,0 +1,8 @@
+var url = 'http://mylogger.io/log';
+
+// log function
+function log(msg) {
+    console.log(msg);
+}
+
+module.exports = log;
