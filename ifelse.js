0ef9136196700c1c274f7bbd2ab7caa1b81330aa
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-07-23 11:15:34 +0500

    conditon in ifelse

---
+++ ifelse.js
@@ -0,0 +1,9 @@
+var CGPA = 3.1;
+
+if(CGPA >= 3.0){
+    console.log("You are eligible for scholarship");   
+} else if(CGPA >= 2.5){
+    console.log("You are not eligible for scholarship");
+} else {
+    console.log("You are not eligible for scholarship");
+}
