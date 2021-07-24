ffc8001961dea51302a6405acccdea4ca90f30a4
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-07-24 16:04:24 +0500

    middle ware in JS

--- ExpressPractice/index.js
+++ ExpressPractice/index.js
@@ -3,7 +3,11 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+
+// middle ware
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
 
