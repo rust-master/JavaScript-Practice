6966fccaed51f0a5365a24c31a602bcbcfd0c036
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-07-24 14:57:48 +0500

    express

--- ExpressPractice/index.js
+++ ExpressPractice/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
@@ -7,7 +8,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.send('About')
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.listen(port, () => {
