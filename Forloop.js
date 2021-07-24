036afe4e921dc047c6d89efb78e4e56875c59ed1
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-07-24 13:17:26 +0500

    triangels

--- Forloop.js
+++ Forloop.js
@@ -3,3 +3,10 @@ for(let i=0; i<=7; i++){
         console.log("#".repeat(i));
     }
 }
+
+let m = 8;
+for(let i=1; i<=7; i++){
+    for(let k=i; k<=i; k++){
+        console.log("#".repeat(k), "*".repeat(m-i));
+    }
+}
