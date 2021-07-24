dac2377bfcbf3174b2ff3e88a8df1ebc9b334be3
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-07-24 16:33:05 +0500

    Blog App

---
+++ Blog/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+// middle ware
+app.use(express.static(path.join(__dirname, "static")))
+
+
+app.listen(port, () => {
+    console.log(`Blog app listening at http://localhost:${port}`)
+})
