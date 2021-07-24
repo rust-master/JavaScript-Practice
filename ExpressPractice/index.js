87926fc70af534219410d0eb7f7d67b5b37a7025
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-07-24 13:51:49 +0500

    changes in express example

--- ExpressPractice/index.js
+++ ExpressPractice/index.js
@@ -3,9 +3,13 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
