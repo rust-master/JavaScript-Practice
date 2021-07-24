ba644a18124e43c140ab13f84dc29da61383e7dd
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-07-24 17:01:38 +0500

    blogs data added

---
+++ Blog/data/blogs.js
@@ -0,0 +1,29 @@
+blogs = [
+    {
+        "id": "1",
+        "title": "Ruby Blog",
+        "content": "Ruby Blog Content",
+    },
+    {
+        "id": "2",
+        "title": "Java Blog",
+        "content": "Java Blog Content",
+    },
+    {
+        "id": "3",
+        "title": "Python Blog",
+        "content": "Python Blog Content",
+    },
+    {
+        "id": "4",
+        "title": "JavaScript Blog",
+        "content": "JavaScript Blog Content",
+    },
+    {
+        "id": "5",
+        "title": "Kotlin Blog",
+        "content": "Kotlin Blog Content",
+    }
+];
+
+module.exports = blogs;
--- Blog/routes/blog.js
+++ Blog/routes/blog.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const blogs = require('../data/blogs')
 
 
 const router = express.Router()
