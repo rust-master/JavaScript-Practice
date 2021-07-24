f53ac5a73ba4d6e03b334b93aa0e3528685120fa
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-07-24 18:26:41 +0500

    formatting

--- Blog/index.js
+++ Blog/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 
 
 const app = express()
