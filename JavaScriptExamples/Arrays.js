c011dea2e6e4a1be3d6de3a73397da3f196ddb7e
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-04 20:11:04 +0500

    arrays code

--- JavaScriptExamples/Arrays.js
+++ JavaScriptExamples/Arrays.js
@@ -3,3 +3,12 @@ selectedColors[3] = 1;
 console.log(selectedColors);
 console.log(typeof selectedColors);
 console.log(selectedColors.length);
+
+let arry = [1,2,3,4,5];
+
+let num = 9;
+
+for(let i=0; i<arry.length; i++){
+    arry[i] += num;
+}
+console.log(arry);
