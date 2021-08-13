81b466b1e59035d9e2339c52d83b097c68111aad
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-13 20:01:12 +0500

    for loop

--- JavaScriptExamples/Forloop.js
+++ JavaScriptExamples/Forloop.js
@@ -1,36 +1,36 @@
-// for(let i=0; i<=7; i++){
-//     for(let j=i; j<=i; j++){
-//         console.log("#".repeat(i));
-//     }
-// }
+for(let i=0; i<=7; i++){
+    for(let j=i; j<=i; j++){
+        console.log("#".repeat(i));
+    }
+}
 
-// let m = 8;
-// for(let i=1; i<=7; i++){
-//     for(let k=i; k<=i; k++){
-//         console.log("#".repeat(k), "*".repeat(m-i));
-//     }
-// }
+let m = 8;
+for(let i=1; i<=7; i++){
+    for(let k=i; k<=i; k++){
+        console.log("#".repeat(k), "*".repeat(m-i));
+    }
+}
 
-// let guest = 6;
-// let chair = 4;
-// let arrangments = 1;
+let guest = 6;
+let chair = 4;
+let arrangments = 1;
 
-// for(let i=0; i<chair; i++){
-//     arrangments *= guest-i;
-// }
+for(let i=0; i<chair; i++){
+    arrangments *= guest-i;
+}
 
-// console.log(`Possible Arrangments: ${arrangments}`);
+console.log(`Possible Arrangments: ${arrangments}`);
 
-// for (let i = 1; i < 1000; i++) {
-//   if (i % 7 == 0) {
-//     console.log(i + " divisible by 7");
-//   }
-// }
-// for (let i = 1; i < 1000; i++) {
-//   if (i % 5 == 0) {
-//     console.log(i + " divisible by 5");
-//   }
-// }
+for (let i = 1; i < 1000; i++) {
+  if (i % 7 == 0) {
+    console.log(i + " divisible by 7");
+  }
+}
+for (let i = 1; i < 1000; i++) {
+  if (i % 5 == 0) {
+    console.log(i + " divisible by 5");
+  }
+}
 
 for(let i=1; i<=5; i++){
   for(let j=1; j<=i; j++) {
